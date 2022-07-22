2fa0fa9b570a5c1dba4f7a8764c9833f9dcfd9f5
haimy <haimanaare@gmail.com>
2022-07-22 19:23:15 +0300

    adding registration

--- b_down/urls.py
+++ b_down/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('down.urls') )
+    path('down/',include('down.urls') )
 ]
--- down/urls.py
+++ down/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+# from views import *
 from . import views
 
 
 urlpatterns = [
     #ergistration urls
-    path("register", views.register_request, name="register")
+    path("register/", views.register_request, name="register"),
+ #landing page
+    path('',views.home , name='home'),
 ]
