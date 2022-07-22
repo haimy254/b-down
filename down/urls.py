c1d9ed40ce8c92bcc70d0771b0b983a773f4341e
haimy <haimanaare@gmail.com>
2022-07-22 19:06:55 +0300

    added register url

--- down/urls.py
+++ down/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
-    #ergistration
+    #ergistration urls
+    path("register", views.register_request, name="register")
 ]
